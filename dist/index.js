774b432f68a16f67b344ddb77757b271324b45e5
Callahan Kovacs <callahan.kovacs@canonical.com>
2022-06-08 12:16:51 -0500

    revert script call

--- dist/index.js
+++ dist/index.js
@@ -3115,7 +3115,7 @@ class SnapcraftBuilder {
         if (this.uaToken) {
             snapcraft = `${snapcraft} --ua-token ${this.uaToken}`;
         }
-        await exec.exec('sg lxd -c "script -q -e -c \'SNAPCRAFT_BUILD_ENVIRONMENT=lxd snapcraft --trace\'"', {
+        await exec.exec('sg', ['lxd', '-c', snapcraft], {
             cwd: this.projectRoot,
             env
         });
