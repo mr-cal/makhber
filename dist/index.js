7e0e0a630db7bb705a9b8ad815d6b17fe0e470c1
Callahan Kovacs <callahan.kovacs@canonical.com>
2022-06-08 08:34:39 -0500

    checkpoint

--- dist/index.js
+++ dist/index.js
@@ -3116,7 +3116,7 @@ class SnapcraftBuilder {
         if (this.uaToken) {
             snapcraft = `${snapcraft} --ua-token ${this.uaToken}`;
         }
-        await exec.exec('script', ['-q', '-e', '-c', 'bash', 'sg', 'lxd', '-c', snapcraft], {
+        await exec.exec('sg', ['lxd', '-c', snapcraft], {
             cwd: this.projectRoot,
             env
         });
